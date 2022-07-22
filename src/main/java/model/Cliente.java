0aef099f0b1099944e43c5a387125d9c7c4b2ae1
Roni Miguel <ronimiguel@gmail.com>
2022-07-22 20:17:06 -0300

    add: getters e setters

--- src/main/java/model/Cliente.java
+++ src/main/java/model/Cliente.java
@@ -14,5 +14,25 @@ public class Cliente {
 	private String nome;
 	@ManyToOne
 	private Endereco endereco;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public Endereco getEndereco() {
+		return endereco;
+	}
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	
 
 }
