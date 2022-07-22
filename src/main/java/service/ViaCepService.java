65cf1741f37c7ca21f920f8ae3350bf9b24acc7a
Roni Miguel <ronimiguel@gmail.com>
2022-07-22 20:43:36 -0300

    add: cliente http para consumo da api

---
+++ src/main/java/service/ViaCepService.java
@@ -0,0 +1,16 @@
+package service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import model.Endereco;
+
+//Client HTTP para o consumo da API do ViaCEP
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+
+	@GetMapping("/{cep}/json/")
+	Endereco consultarCep(@PathVariable("cep") String cep);
+}
